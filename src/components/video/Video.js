8e9ac1667753f1fc87f29b02c7cf83dfc771be19
Maria Sui <maria.sui51@gmail.com>
2023-05-21 15:43:45 -0400

    added video list tablet view

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,9 +1,9 @@
 import './_video.scss'
 
 function Video(props) {
-  function truncateString(str){
-    return str.length>40 ? str.substring(0,35) + "...": str;
-  }
+  // function truncateString(str){
+  //   return str.length>40 ? str.substring(0,35) + "...": str;
+  // }
 
 
 
@@ -15,7 +15,7 @@ function Video(props) {
             <img className="video__image" src={props.image} alt="video images"></img>
         </div>
         <div className="video__wrapper--right">
-            <h3 className="video__title">{truncateString(props.title)}</h3>
+            <h3 className="video__title">{props.title}</h3>
             <p className="video__author">{props.author}</p>
         </div>
     </>
