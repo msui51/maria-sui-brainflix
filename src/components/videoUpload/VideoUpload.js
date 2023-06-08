30038578eca157f169cdd936563fff1d2a5c1fc1
Maria Sui <maria.sui51@gmail.com>
2023-06-08 10:36:27 -0400

    restructured upload page

--- src/components/videoUpload/VideoUpload.js
+++ src/components/videoUpload/VideoUpload.js
@@ -37,14 +37,13 @@ function VideoUpload() {
   return (
     <main className="upload">
         <h1 className="upload__title">Upload Video</h1>
-        <div className="upload__wrapper">
-           
-            <div className="upload__image-wrapper">
-                <label className="upload__label" for="image">VIDEO THUMBNAIL</label>
-                <img className="upload__image" src={thumbnail} alt="blue bike"></img>
-            </div>
-            <div className="upload__form-wrapper">
-                <form className="upload__form" onSubmit={handleSubmit}>
+        <form className="upload__wrapper" onSubmit={handleSubmit}>
+           <div className="upload__image-form-wrapper">
+                <div className="upload__image-wrapper">
+                    <label className="upload__label" for="image">VIDEO THUMBNAIL</label>
+                    <img className="upload__image" src={thumbnail} alt="blue bike"></img>
+                </div>
+                <div className="upload__form-wrapper">
                     <label className="upload__label" 
                         for="title">
                         TITLE YOUR VIDEO
@@ -69,13 +68,13 @@ function VideoUpload() {
                         value={text}
                         onChange={textChangeHandler}>    
                     </input>
-                    <div className='upload__button-wrapper'>
-                        <button className="upload__button" type="submit">PUBLISH</button>
-                        <button className="upload__button upload__button--different">CANCEL</button>
-                    </div>
-                </form>
+                </div>
             </div>
-        </div>
+            <div className='upload__button-wrapper'>
+                <button className="upload__button" type="submit">PUBLISH</button>
+                <button className="upload__button upload__button--different">CANCEL</button>
+            </div>
+        </form>
     </main>
   )
 }
